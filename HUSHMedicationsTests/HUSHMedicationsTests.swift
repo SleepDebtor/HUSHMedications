6b55b54ca5dc8be3ae21addf1a4ef984ee7afa5f
Michael Lazar <mike@lazarmedicalgroup.com>
2025-09-20 18:04:02 -0400

    Initial Commit

---
+++ HUSHMedicationsTests/HUSHMedicationsTests.swift
@@ -0,0 +1,16 @@
+//
+//  HUSHMedicationsTests.swift
+//  HUSHMedicationsTests
+//
+//  Created by Michael Lazar on 9/20/25.
+//
+
+import Testing
+
+struct HUSHMedicationsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
